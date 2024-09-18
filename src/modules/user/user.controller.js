74021c5e7d0a0197f2a28b61ea1b97488a8911ef
Aya Abdelsamed <aya.abdelsamed11pp@gmail.com>
2024-09-18 04:54:30 +0300

    Sending Email

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,9 +1,12 @@
 import { userModel } from "../../../databases/models/user.model.js"
+import { sendEmail } from "../../emails/sendEmail.js"
 
 
 const signup =async(req,res)=>{
     
     await userModel.insertMany(req.body)
+
+    sendEmail()
     res.json({message:"success"})
 }
 

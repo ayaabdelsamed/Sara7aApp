bd0da89e6c784d794dcf4ea18a8d28918cb834de
Aya Abdelsamed <aya.abdelsamed11pp@gmail.com>
2024-09-29 07:41:36 +0300

    Setup joi

--- src/middleware/checkEmail.js
+++ src/middleware/checkEmail.js
@@ -4,10 +4,10 @@ import { AppError } from "../utils/appError.js"
 
 export const checkEmail= async(req,res,next)=>{
     
-    let user = await userModel.findOne({email:req.body.email})
-    if(user) return next(new AppError("email already exists.",409))
+    // let user = await userModel.findOne({email:req.body.email})
+    // if(user) return next(new AppError("email already exists.",409))
 
-    req.body.password=bcrypt.hashSync(req.body.password,8)
+    // req.body.password=bcrypt.hashSync(req.body.password,8)
 
     next()
 }
--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -5,14 +5,35 @@ import { sendEmail } from "../../emails/sendEmail.js"
 import { catchError } from "../../middleware/catchError.js"
 import { AppError } from "../../utils/appError.js"
 
+import Joi from "joi"
 
+const signupSchema = Joi.object({
+    name: Joi.string().min(2).max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(/^[A-Z][a-z0-9#@]{8,40}$/),
+    rePassword: Joi.valid(Joi.ref('password')).required(),
+    age: Joi.number().integer().min(10).max(80).required()
 
-
-const signup =catchError(async(req,res)=>{
-    await userModel.insertMany(req.body)
-    sendEmail(req.body.email)
-    res.json({message:"success"})
 })
+
+
+
+
+const signup =catchError(async(req,res,next)=>{
+    const {error} = signupSchema.validate(req.body)
+    console.log(error);
+    if(!error){
+        await userModel.insertMany(req.body)
+        //let token = jwt.sign({email: req.body.email},)
+        res.json({message:"success"})
+    }else{
+        res.json({message:"success",error : error.details})
+    }
+    
+    //sendEmail(req.body.email)
+    
+})
+
 
 const signin =catchError(async(req,res,next)=>{
     let user = await userModel.findOne({email:req.body.email})
